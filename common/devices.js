3b736de56e8620450aaf3c3eb530ec2e41b89260
torwood3 <torwood@hotmail.fr>
2017-02-23 11:40:37 +0100

    change ping to tcp-ping

--- common/devices.js
+++ common/devices.js
@@ -1,9 +1,7 @@
 /**
  * Created by pamas on 03/02/2017.
  */
-
-
-var ping = require ("ping");
+var tcpp = require('tcp-ping');
 var wol = require('wake_on_lan');
 var request = require('superagent');
 var Datastore = require('nedb')
@@ -68,10 +66,10 @@ var devicePing =  function(req, res) {
 	db.find({_id: req.params.id}, function (err, device) {
 		if(err) 			res.status(500);
 
-		ping.promise.probe(device.ip).then(function (result) {
-			db.update({ _id: req.params.id }, { $set: { status: result.alive.toString()} }, function (err, numReplaced) {
+		tcpp.probe(device.ip, function (err, available) {
+			db.update({ _id: req.params.id }, { $set: { status: available.toString()} }, function (err, numReplaced) {
 				if(err) 			res.status(500);
-				res.json({isAlive: result.alive.toString()});
+				res.json({isAlive: available.toString()});
 			});
 		});
 	})
@@ -112,14 +110,14 @@ setInterval(function() {
 		console.log("Devices (" + devices.length +")");
 		devices.forEach(function(device) {
 			console.log(device);
-			ping.promise.probe(device.ip).then(function (result) {
-				console.log(device.ip + " - " + result.alive);
-				db.update({ _id: device._id }, { $set: { status: result.alive.toString()} }, function (err, numReplaced) {
+			tcpp.probe(device.ip, function (err, available) {
+				console.log(device.ip + " - " + available);
+				db.update({ _id: device._id }, { $set: { status: available.toString()} }, function (err, numReplaced) {
 					if(err) 		console.log(device._id + " - ping error");
 					console.log(device._id + " - ping ok");
-
 				});
 			});
 		});
 	})
 }, 1000 * 5);
+
